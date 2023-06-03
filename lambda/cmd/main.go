c1c103f02c5535295a914a68941634a87c4990f5
k0825 <kazukkiyo@icloud.com>
2023-06-03 17:06:10 +0900

    サンプルで出力してみる

--- lambda/cmd/main.go
+++ lambda/cmd/main.go
@@ -3,18 +3,22 @@ package main
 import (
 	"context"
 	"go-lambda-slack-notifier/slack"
+	"log"
 	"os"
 
+	"github.com/aws/aws-lambda-go/events"
 	"github.com/aws/aws-lambda-go/lambda"
 )
 
-type MyEvent struct {
-	Name string `json:"name"`
-}
-
-func HandleRequest(ctx context.Context, name MyEvent) (string, error) {
+func HandleRequest(ctx context.Context, s3Event events.S3Event) (string, error) {
 	webhookUrl := os.Getenv("SLACK_WEBHOOK_URL")
-	if slack.PostMessage(webhookUrl, "Hello, "+name.Name) != nil {
+
+	for _, record := range s3Event.Records {
+		s3rec := record.S3
+		log.Printf("[%s - %s] Bucket = %s, Key = %s \n", record.EventSource, record.EventTime, s3rec.Bucket.Name, s3rec.Object.Key)
+	}
+
+	if slack.PostMessage(webhookUrl, "Hello") != nil {
 		return "Error", nil
 	} else {
 		return "Success", nil
