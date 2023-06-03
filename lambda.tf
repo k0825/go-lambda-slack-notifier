c403ea81aaef3f741f3feb86316f186c2dc3637a
k0825 <kazukkiyo@icloud.com>
2023-06-03 15:54:35 +0900

    環境変数追加

--- lambda.tf
+++ lambda.tf
@@ -5,6 +5,12 @@ resource "aws_lambda_function" "lambda" {
   handler          = "main"
   runtime          = "go1.x"
   source_code_hash = data.archive_file.lambda.output_base64sha256
+
+  environment {
+    variables = {
+      SLACK_WEBHOOK_URL = var.slack_webhook_url
+    }
+  }
 }
 
 resource "null_resource" "default" {
