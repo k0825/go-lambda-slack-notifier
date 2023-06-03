91699348948e276b6e350326536161ee91da1f40
k0825 <kazukkiyo@icloud.com>
2023-06-03 14:12:47 +0900

    TerraformでLambdaを定義

---
+++ lambda.tf
@@ -0,0 +1,25 @@
+resource "aws_lambda_function" "lambda" {
+  function_name    = "go-lambda-sample"
+  filename         = "./lambda/archive/main.zip"
+  role             = aws_iam_role.lambda_role.arn
+  handler          = "main"
+  runtime          = "go1.x"
+  source_code_hash = data.archive_file.lambda.output_base64sha256
+}
+
+resource "null_resource" "default" {
+  triggers = {
+    always_run = timestamp()
+  }
+  provisioner "local-exec" {
+    command = "GOOS = linux GOARCH = amd64 go build -o ./lambda/build/main ./lambda/src/lambda-sample/main.go"
+  }
+}
+
+data "archive_file" "lambda" {
+  type        = "zip"
+  source_file = "./lambda/build/main"
+  output_path = "./lambda/archive/main.zip"
+
+  depends_on = [null_resource.default]
+}
