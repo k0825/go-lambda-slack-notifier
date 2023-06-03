c4cb0d42896cbc07326eef5ecb321450d4c04df5
k0825 <kazukkiyo@icloud.com>
2023-06-03 14:34:20 +0900

    ポリシーの場所を変更

--- iam.tf
+++ iam.tf
@@ -1,11 +1,11 @@
 resource "aws_iam_role" "lambda_role" {
   name               = "go-lambda-slack-notifier-role"
-  assume_role_policy = file("iam/lambda-assume-role.json")
+  assume_role_policy = file("policies/lambda-assume-role.json")
 }
 
 resource "aws_iam_policy" "lambda_policy" {
   name   = "go-lambda-slack-notifier-policy"
-  policy = file("iam/lambda-policy.json")
+  policy = file("policies/lambda-policy.json")
 }
 
 resource "aws_iam_role_policy_attachment" "lambda_policy_attachment" {
