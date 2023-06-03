d6f20052a79a24b31ae2847532b5d1208dddb6a0
k0825 <kazukkiyo@icloud.com>
2023-06-03 16:53:33 +0900

    lambda名修正

--- s3.tf
+++ s3.tf
@@ -8,7 +8,7 @@ resource "aws_s3_bucket_notification" "bucket_notification" {
   bucket = aws_s3_bucket.sample_bucket.id
 
   lambda_function {
-    lambda_function_arn = aws_lambda_function.sample_lambda.arn
+    lambda_function_arn = aws_lambda_function.lambda.arn
     events              = ["s3:ObjectCreated:*"]
     filter_prefix       = "sample/"
   }
